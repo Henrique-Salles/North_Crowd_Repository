24fb886cbf46e6b2bf481605aabf73ff984149da
Henrique-Salles <henriquesalles1520@gmail.com>
2022-11-26 15:22:25 -0300

    Initial Commit

--- system js/principal.js
+++ system js/principal.js
@@ -18,35 +18,35 @@ function excluir() {
     }
 }
 
-function addCliente() {
-    alert("Cliente adicionado com sucesso!")
-}
+// function addCliente() {
+//     alert("Cliente adicionado com sucesso!")
+// }
 
 function addProd() {
     alert("Produto adicionado com sucesso!")
 }
 
-function editCliente() {
-    alert("Cliente editado com sucesso!")
-}
+// function editCliente() {
+//     alert("Cliente editado com sucesso!")
+// }
 
-function editCli() {
-    window.location = "../system pages/clientes-edit.html"
-}
+// function editCli() {
+//     window.location = "../system pages/clientes-edit.html"
+// }
 
-function excluirFunc() {
-    var e = confirm("Tem certeza que deseja excluir este funcionário?");
-    if (e == true) {
-        alert("Funcionário excluído com sucesso.")
-    }
-}
+// function excluirFunc() {
+//     var e = confirm("Tem certeza que deseja excluir este funcionário?");
+//     if (e == true) {
+//         alert("Funcionário excluído com sucesso.")
+//     }
+// }
 
-function excluirVenda() {
-    var v = confirm("Tem certeza que deseja excluir esta venda?");
-    if (v == true) {
-        alert("Venda excluída com sucesso.")
-    }
-}
+// function excluirVenda() {
+//     var v = confirm("Tem certeza que deseja excluir esta venda?");
+//     if (v == true) {
+//         alert("Venda excluída com sucesso.")
+//     }
+// }
 
 function excluirProduto() {
     var p = confirm("Tem certeza que deseja excluir este produto?");
@@ -55,49 +55,49 @@ function excluirProduto() {
     }
 }
 
-function addFuncionario() {
-    alert("Funcionário adicionado com sucesso!")
-}
+// function addFuncionario() {
+//     alert("Funcionário adicionado com sucesso!")
+// }
 
-function editFuncionario() {
-    alert("Funcionário editado com sucesso!")
-}
+// function editFuncionario() {
+//     alert("Funcionário editado com sucesso!")
+// }
 
-function addVenda() {
-    alert("Venda registrada com sucesso!")
-}
+// function addVenda() {
+//     alert("Venda registrada com sucesso!")
+// }
 
-function editFunc() {
-    window.location = "../system pages/funcionarios-edit.html"
-}
+// function editFunc() {
+//     window.location = "../system pages/funcionarios-edit.html"
+// }
 
-function editarEntrega() {
-        window.location = "../system pages/entregas-edit.html"
-    }
+// function editarEntrega() {
+//         window.location = "../system pages/entregas-edit.html"
+//     }
 
-    function editEntrega() {
-        alert("Entrega editada com sucesso!")
-    }
+//     function editEntrega() {
+//         alert("Entrega editada com sucesso!")
+//     }
 
     function editarProd() {
         alert("Produto editado com sucesso!")
     }
 
-    function vendas() {
-        window.location = "../system pages/vendas.html"
-    }
+    // function vendas() {
+    //     window.location = "../system pages/vendas.html"
+    // }
     function estoque() {
         window.location = "../system pages/estoque.html"
     }
-    function clientes() {
-        window.location = "../system pages/clientes.html"
-    }
-    function funcionarios() {
-        window.location = "../system pages/funcionarios.html"
-    }
-    function entregas() {
-        window.location = "../system pages/entregas.html"
-    }
+    // function clientes() {
+    //     window.location = "../system pages/clientes.html"
+    // }
+    // function funcionarios() {
+    //     window.location = "../system pages/funcionarios.html"
+    // }
+    // function entregas() {
+    //     window.location = "../system pages/entregas.html"
+    // }
 
     function estoqueEdi() {
         window.location = "../system pages/estoque-edit.html"
